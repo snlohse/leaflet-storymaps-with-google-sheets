fa23f36348646f7ead43740e513f9fd719d5bd6e
snlohse <snlohse@gmail.com>
2025-03-11 00:05:40 -0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ogqBDP19E9qOwUMCJ4-FxjVR6N9Rvyx2mInlINIuKkY/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyB6yuAXk4YT_Dmpx3pRr9vzFisuixErm9c';
